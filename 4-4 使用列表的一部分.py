638423b1e00035903fed7a4e7ee17e220f4c7e8d
David-LinGuoguo <www814099377@qq.com>
2022-01-07 09:27:43 +0800

    学到了4-4

---
+++ 4-4 使用列表的一部分.py
@@ -0,0 +1,27 @@
+# players = ['charles', 'martina', 'michael', 'florance', 'eli']
+# print(players[0:3])
+# print(players[1:4])
+# print(players[:4])
+# print(players[2:])
+# print(players[-3:])
+# print(players[0::2])
+
+# print("Here are the first three players on my team:")
+# for player in players[0:3]:
+#     print(player.title())
+
+# 复制列表
+my_foods = ['pizza', 'falafel', 'carrot cake'] #falafel 炸豆丸子 n.
+# friends_food = []
+# for food in my_foods:
+#     friends_food.append(food)
+# print(friends_food)
+# 上面是我自己写的，不是书本上的方法
+
+friends_food = my_foods[:]
+print("My favorites food are :")
+print(my_foods)
+
+print("\nMy friends' favorite food are :")
+print(friends_food)
+
