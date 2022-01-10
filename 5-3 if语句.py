f820b5ebafb8e08f36f12ed66727b75d0cd73c5a
David-LinGuoguo <www814099377@qq.com>
2022-01-10 13:53:00 +0800

    2022.1.10 afternoon

---
+++ 5-3 if语句.py
@@ -0,0 +1,50 @@
+# age = 19
+# if age >= 18:
+#     print("You are old enough to vote")
+
+# age = 17 
+# if age >= 18:
+#     print("You are old enough to vote")
+# else:
+#     print("Sorry, you are too young to vote")
+
+# age = 12
+# if age < 4:
+#     print("Your admission cost is $0.")
+# elif age < 18:
+#     print("Your admision cost is $25.")
+# else:
+#     print("Your admision cost is $40.")
+
+# age = 12
+# if age < 4:
+#     price = 0
+# elif age < 18:
+#     price = 25
+# elif age <65:
+#     price = 40
+# else:
+#     price = 20
+# print(f"Your admission cost is ${price}")
+
+# age = 65
+# if age < 4:
+#     price = 0
+# elif age < 18:
+#     price = 25
+# elif age < 65:
+#     price = 40
+# elif age >= 65:
+#     price = 20
+# print(f"Your admission cost is ${price}")
+
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushroom.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese")
+print("\nFinish making your pizza")
+
