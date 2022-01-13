b4baf81ce3c65bd1c6859d1df88166cbf8159d13
David-LinGuoguo <www814099377@qq.com>
2022-01-14 01:39:35 +0800

    Bravo！

---
+++ 7-1 函数input( )的工作原理.py
@@ -0,0 +1,9 @@
+# message = input("Tell me something, and I will repeat it back to you.\n")
+# print(message)
+
+# name = input("Pleae enter your name:")
+# print(f"\nHello,{name.title()}")
+
+age = input("How old are you?")
+age = int(age)
+print(f"You are {age}-year-old!")
---
+++ 7-2 while循环简介.py
@@ -0,0 +1,43 @@
+# current_number = 1
+# while current_number <= 5:
+#     print(current_number)
+#     current_number += 1
+
+# prompt = "\nTell me something, and I will repeat it back to you:"
+# prompt += "\nEnter 'quit' to end the program."
+# message = ""
+# while message != 'quit':
+#     message = input(prompt)
+#     if message != "quit":
+#         print(message)
+
+# active = True
+# while active:
+#     message = input("prompt")
+#     if message.title() == False:
+#         active = False
+#     elif message.title() != False:
+#         print(message)
+
+# prompt = "\nPlease Enter the name of a city you have visited:"
+# prompt += "\n(Enter 'quit' when you are finished.)"
+
+# while True:
+#     city = input(prompt)
+#     if city == 'quit':
+#         break
+#     else:
+#         print(f"I 'd love to go to {city.title()}")
+
+# current_number = 0
+# while current_number < 10:
+#     current_number += 1
+#     if current_number % 2 ==0:
+#         continue
+
+#     print(current_number)
+
+x = 1
+while x <= 5:
+    print(x)
+    x += 1
