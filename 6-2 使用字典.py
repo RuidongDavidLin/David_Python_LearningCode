5636b6d328e82a9607564eae38a80f15a9a53890
David-LinGuoguo <www814099377@qq.com>
2022-01-13 10:51:29 +0800

    6-2

--- 6-2 使用字典.py
+++ 6-2 使用字典.py
@@ -14,18 +14,34 @@
 # alien_0['color'] = 'yellow'
 # print(f"The alien is {alien_0['color']}")
 
-alien_0 ={'x_position':0, 'y_position':25, 'speed':'medium'}
-print(alien_0)
-print(f"Origin x_position:{alien_0['x_position']}")
+# alien_0 ={'x_position':0, 'y_position':25, 'speed':'medium'}
+# print(alien_0)
+# print(f"Origin x_position:{alien_0['x_position']}")
 
-if alien_0['speed'] == 'slow':
-    x_increment = 1
-elif alien_0['speed'] == 'medium':
-    x_increment = 2
-else:
-    x_increment = 3
-alien_0['x_position'] = alien_0['x_position'] + x_increment
-print(f"New x_position: {alien_0['x_position']}")
+# if alien_0['speed'] == 'slow':
+#     x_increment = 1
+# elif alien_0['speed'] == 'medium':
+#     x_increment = 2
+# else:
+#     x_increment = 3
+# alien_0['x_position'] = alien_0['x_position'] + x_increment
+# print(f"New x_position: {alien_0['x_position']}")
 
-del alien_0['y_position']
-print(f"{alien_0}")
+# del alien_0['y_position']
+# print(f"{alien_0}")
+
+# favorite_language = {
+#     'jen': 'python',
+#     'sarah': 'c',
+#     'edward': 'ruby',
+#     'phil': 'python',
+# }
+
+# language = favorite_language['sarah']
+# print(f"Sarah's favorite language is {language}")
+
+# alien_0 = {'color': 'green', 'speed': 'slow'}
+# point_value = alien_0.get('point', 'No point value assigned.')
+# alien_0['point'] = point_value
+# print(alien_0['point'])
+
