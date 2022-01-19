57dad11e9aee8f9b0cfbc02d78f5d27a86e2f279
RuiodngDavidLin <www814099377@qq.com>
2022-01-19 11:45:57 +0800

    好久没见，甚是想念！

---
+++ 8-1 定义函数.py
@@ -0,0 +1,13 @@
+# def greet_user():
+#     """
+#     显示简单的问候语
+#     """
+#     print("Hello!")
+
+# greet_user()
+
+def greet_user(username):
+    """显示简单的问候语"""
+    print(f"Hello, {username.title()}")
+    
+greet_user("david")
